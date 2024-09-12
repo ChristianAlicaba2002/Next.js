e61dd76d1dad5c29b35b66c92ec4a61465e272af
ChristianAlicaba_Developer <131524430+ChristianAlicaba2002@users.noreply.github.com>
2024-09-12 11:36:47 -1100

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Home from '../page'
+
+const Shoppage = () => {
+  return (
+        <div>
+            <Home></Home>
+            <h1>This is Shop page</h1>
+        </div>
+  )
+}
+
+export default Shoppage
